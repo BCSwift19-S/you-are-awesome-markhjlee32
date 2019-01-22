f2b135cf3e7ae127bdd45699a29ed51970f7cef8
Mark Lee <marklee@marks-mbp-2.bc.edu>
2019-01-22 01:22:33 -0500

    Initial Commit

---
+++ YouAreAwesome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouAreAwesome
+//
+//  Created by Mark Lee on 1/22/19.
+//  Copyright © 2019 Mark Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
